92b65d1f37537d5a474e5bc93e50d9ec2d296d1c
Bình Kiều Thị <kbinh780@gmail.com>
2021-11-06 23:01:07 +0700

    Đã Update database

--- eshopsolution_data/entitys/AppRole.cs
+++ eshopsolution_data/entitys/AppRole.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace eshopsolution_data.entitys
 {
-    public class AppRole
+    public class AppRole:IdentityRole<Guid>
     {
         public string Description { get; set; }
     }
--- eshopsolution_data/entitys/AppUser.cs
+++ eshopsolution_data/entitys/AppUser.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace eshopsolution_data.entitys
 {
-    public class AppUser 
+    public class AppUser :IdentityUser<Guid>
     {
         public string FirstName { get; set; }
 
